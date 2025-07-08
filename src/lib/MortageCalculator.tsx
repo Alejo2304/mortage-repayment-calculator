bf39d4e1e130abb3702fc23223b7fd12ff66db6e
Alejo2304 <ojeda.alejandro.2304@gmail.com>
2025-07-08 18:03:26 -0500

    MortageCalculator.tsx | Handles the logic of the calculator.

---
+++ src/lib/MortageCalculator.tsx
@@ -0,0 +1,7 @@
+export function CalculateRepayment(amount: number, termYears: number, interestRate: number) {
+
+}
+
+export function CalculateInterest(amount: number, termYears: number, interestRate: number) {
+
+}
