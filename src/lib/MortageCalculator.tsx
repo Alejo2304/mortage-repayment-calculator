adead31450487035fdadc5a1a615daa33a26aa6a
Alejo2304 <ojeda.alejandro.2304@gmail.com>
2025-07-09 12:54:10 -0500

    MortageCalculator.tsx | Added functions CalculateMonthlyPaymentFixedRate & CalcultaTotalPaymentFixedRate
    Functions receive amount, termYears and interestRate and return the monthly payment and the total payment at then end of the term.

--- src/lib/MortageCalculator.tsx
+++ src/lib/MortageCalculator.tsx
@@ -1,7 +1,28 @@
-export function CalculateRepayment(amount: number, termYears: number, interestRate: number) {
+function CalculateMonthlyInterestRate(interestRate: number): number{
+    return((interestRate/100) / 12);
+    }
 
+function CalculateNumberOfMonthlyPayments(termYears: number): number{
+    return (termYears * 12);
 }
 
-export function CalculateInterest(amount: number, termYears: number, interestRate: number) {
+export function CalculateMonthlyPaymentFixedRate(amount: number, termYears: number, interestRate: number): number{
+    const r: number = CalculateMonthlyInterestRate(interestRate);
+    const n: number = CalculateNumberOfMonthlyPayments(termYears);
+    const onePlusRPowerN = Math.pow(1 + r, n);
 
+    if (amount <= 0 || termYears <= 0 || interestRate < 0) {
+        throw new Error("Invalid parameters");
+    }
+
+    if (r === 0) {
+        return amount / n; // Sin intereses
+    }
+
+    return ((amount * r * onePlusRPowerN) / (onePlusRPowerN - 1));
+}
+
+export function CalcultaTotalPaymentFixedRate(amount: number, termYears: number, interestRate: number): number{
+    const monthlyPayment = CalculateMonthlyPaymentFixedRate(amount, termYears, interestRate);
+    return (monthlyPayment * CalculateNumberOfMonthlyPayments(termYears));
 }
