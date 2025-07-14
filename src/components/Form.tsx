a2916782b45e1500ee1f8ea6d0a71617fb00d9e9
Alejo2304 <ojeda.alejandro.2304@gmail.com>
2025-07-13 22:55:41 -0500

    Added Styles for the form | Refactorized inputs on FormComponents
    Replaced the TextInput component with CurrencyInput and NumberInput, fixed styles for all inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,5 +1,5 @@
 import type { FormData } from "../types.ts";
-import {Button, AmountInput, TermInput, RateInput, RadioGroup} from "./FormComponents.tsx"
+import {Button, CurrencyInput, NumberInput, RadioGroup} from "./FormComponents.tsx"
 
 
 type Props = {
@@ -10,22 +10,30 @@ type Props = {
 
 export default function Form({formData, setFormData, handleSubmit}:Props): React.ReactElement{
     return(
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit} className="p-5 space-y-5">
 
-            <AmountInput
+            <h1 className="font-jakarta font-bold text-slate-900 text-xl">Mortgage Calculator</h1>
+
+            <CurrencyInput
                 step="1"
                 value={formData.amount}
                 onChange={e => setFormData({ ...formData, amount: e.target.value, formStatus: "typing" })}
             />
 
-            <TermInput
+            <NumberInput
+                label="Mortgage Term"
+                id="termInput"
                 step="1"
+                unit="years"
                 value={formData.termYears}
                 onChange={e => setFormData({ ...formData, termYears: e.target.value, formStatus: "typing" })}
             />
 
-            <RateInput
+            <NumberInput
+                label="Interest Rate"
+                id="interestInput"
                 step="1.0"
+                unit="%"
                 value={formData.interestRate}
                 onChange={e => setFormData({ ...formData, interestRate: e.target.value, formStatus: "typing" })}
             />
--- src/components/FormComponents.tsx
+++ src/components/FormComponents.tsx
@@ -11,6 +11,7 @@ type TextInputProps = {
   id?: string;
   type?: string;
   step?: string;
+  unit?: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
@@ -27,20 +28,21 @@ export function Button({calculationType, formStatus}: ButtonProps): React.ReactE
     const label: string = calculationType === "InterestOnly" ? "Calculate Interests" : "Calculate Repayments";
     const isDisabled: boolean = formStatus === "empty" ? true : false;
     return(
-        <button type="submit" disabled={isDisabled} className="flex justify-center items-center bg-lime rounded-xl">
-            <img src={calculatorIcon} alt="Calculator icon" className="flex-1/4 h-[1.5lh]"></img> 
-            <span className="flex-3/4 font-jakarta text-slate-900">{label}</span>
+        <button type="submit" disabled={isDisabled} className="flex justify-center items-center bg-lime-900 disabled:bg-lime-1100 hover:bg-lime-950 active:bg-lime-1000 rounded-3xl py-2 px-5 space-x-2 w-full">
+            <img src={calculatorIcon} alt="Calculator icon" className="h-[0.8lh]"></img> 
+            <span className="font-jakarta font-bold text-slate-900 text-nowrap">{label}</span>
         </button>
     );
 }
 
-export function AmountInput({ label="Mortgage Amount", id="amountInput", type = "number", step, value, onChange }: TextInputProps) {
+export function CurrencyInput({ label="Mortgage Amount", id="amountInput", type = "number",unit="£", step, value, onChange }: TextInputProps) {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-      <div className="flex items-center border rounded-lg overflow-hidden bg-white">
-        <span className="px-3 py-2 bg-slate-300 text-slate-500 font-bold">£</span>
+      <label htmlFor={id} className="font-jakarta text-slate-700">{label}</label>
+      <div className="flex items-center border border-slate-500 rounded-lg overflow-hidden bg-white">
+        <span className="px-3 py-2 bg-slate-100 text-slate-500 font-bold">{unit}</span>
         <input
+          className="w-full px-4 focus:outline-none font-jakarta font-semibold text-slate-900"
           type={type}
           id={id}
           step={step}
@@ -52,62 +54,53 @@ export function AmountInput({ label="Mortgage Amount", id="amountInput", type = 
   );
 }
 
-export function TermInput({ label="Mortgage Term", id="termInput", type ="number", step, value, onChange }: TextInputProps) {
+export function NumberInput({ label="Number Input", id="numberInput", type ="number", unit="Number", step, value, onChange }: TextInputProps) {
   return (
     <>
-      <label htmlFor={id}>{label}</label>
-      <div className="flex items-center border rounded-lg overflow-hidden bg-white">
-        <input
+      <label htmlFor={id} className="font-jakarta text-slate-700">{label}</label>
+      <div className="flex items-center border  border-slate-500 rounded-lg overflow-hidden bg-white">
+        <input 
+          className="w-full px-4 focus:outline-none font-jakarta font-semibold text-slate-900"
           type={type}
           id={id}
           step={step}
           value={value}
           onChange={onChange}
         />
-        <span className="px-3 py-2 bg-slate-300 text-slate-500 font-bold">years</span>
+        <span className="px-3 py-2 bg-slate-100 text-slate-500 font-bold">{unit}</span>
       </div>
     </>
   );
 }
-
-export function RateInput({ label="Interest Rate", id="rateInput", type ="number", step, value, onChange }: TextInputProps) {
-  return (
-    <>
-      <label htmlFor={id}>{label}</label>
-      <div className="flex items-center border rounded-lg overflow-hidden bg-white">
-        <input
-          type={type}
-          id={id}
-          step={step}
-          value={value}
-          onChange={onChange}
-        />
-        <span className="px-3 py-2 bg-slate-300 text-slate-500 font-bold">%</span>
-      </div>
-    </>
-  );
-}
-
-
-
 
 export function RadioGroup({ legend, name, options, selectedValue, onChange }: RadioGroupProps) {
   return (
     <fieldset>
-      <legend>{legend}</legend>
-      {options.map(opt => (
-        <label key={opt.value} htmlFor={opt.value}>
-          <input
-            type="radio"
-            id={opt.value}
-            name={name}
-            value={opt.value}
-            checked={selectedValue === opt.value}
-            onChange={() => onChange(opt.value)}
-          />
-          {opt.label}
-        </label>
-      ))}
+      <legend className="font-jakarta text-slate-700">{legend}</legend>
+      <div className="space-y-2">
+        {options.map(opt => {
+          const inputId = `${name}-${opt.value}`;
+          const isSelected = selectedValue === opt.value;
+          const styleDivSelected = isSelected ? "bg-lime-200 border-lime-500" : "bg-white";
+          const styleInputSelected = isSelected ? "p-1.5 bg-lime-900 rounded-full ring-lime-900 ring-2 ring-offset-1" : "p-1.5 border rounded-full";
+          return (
+            <div key={opt.value} className={`w-full border px-2 py-1.5 rounded-lg overflow-hidden ${styleDivSelected}`}>
+                <label htmlFor={inputId} className="flex items-center space-x-3 ">
+                <input
+                  className={`appearance-none ${styleInputSelected}`}
+                  type="radio"
+                  id={inputId}
+                  name={name}
+                  value={opt.value}
+                  checked={isSelected}
+                  onChange={() => onChange(opt.value)}
+                />
+                <span className="font-jakarta font-semibold text-slate-900">{opt.label}</span>
+               </label>
+            </div>
+          );
+        })}
+      </div>
     </fieldset>
   );
 }
